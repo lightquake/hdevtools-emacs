1ec4a0439a144be628c3cd6aa9aaf35c7d840c41
Kata <lightquake@amateurtopologist.com>
2013-12-17 23:03:48 -0600

    Get all type information into a struct list.

--- hdevtools.el
+++ hdevtools.el
@@ -1,4 +1,7 @@
 ;; hdevtools --- hdevtools integration with emacs
+
+;; Package-Requires: ((cl-lib "0.3") (dash "2.3.0"))
+
 ;;; Commentary:
 
 ;; This package currently only supports type information; for syntax
@@ -7,14 +10,15 @@
 ;;; Code:
 
 (require 'cl-lib)
+(require 'dash)
 
-(cl-defstruct (hdevtools//type-info (:constructor hdevtools//make-type-info))
+(cl-defstruct (hdevtools/type-info (:constructor hdevtools/make-type-info))
   start end type)
 
-(defun hdevtools//get-type-info ()
+(defun hdevtools/get-type-info ()
   "Get all type information for identifiers containing point."
-
   (let* ((line-number (line-number-at-pos))
+         ;; emacs numbers columns from 0, Haskell numbers columns from 1.
          (col-number (1+ (current-column)))
          (file-name (buffer-file-name))
          (hdevtools-buffer (get-buffer-create "*hdevtools*")))
@@ -23,8 +27,7 @@
       (call-process "hdevtools" nil t nil "type" file-name
                     (number-to-string line-number)
                     (number-to-string col-number)))
-    (hdevtools//parse-type-info (current-buffer) hdevtools-buffer)
-    ))
+    (hdevtools//parse-type-info (current-buffer) hdevtools-buffer)))
 
 (defun hdevtools//parse-type-info (haskell-buffer hdevtools-buffer)
   "Parse type information for HASKELL-BUFFER out of HDEVTOOLS-BUFFER.
@@ -32,17 +35,28 @@
 Assumes the current buffer does actually have type information."
   (with-current-buffer hdevtools-buffer
     (goto-char (point-min))
+    (-filter 'identity (cl-loop collect (hdevtools//parse-single-type-info
+                                        haskell-buffer hdevtools-buffer)
+                               until (eobp) do (forward-line 1)))))
+
+(defun hdevtools//parse-single-type-info (haskell-buffer hdevtools-buffer)
+  "Parse a single line of type info for HASKELL-BUFFER out of HDEVTOOLS-BUFFER.
+
+Assumes point is at the start of the line.  If the line contains
+no type information, returns nil."
+  (if (not (looking-at "[0-9]")) nil
     (let* ((parse-number (lambda () (forward-word) (thing-at-point 'number)))
            (line-start (funcall parse-number))
-           (col-start (- (funcall parse-number) 1))
+           (col-start (1- (funcall parse-number)))
            (line-end (funcall parse-number))
-           (col-end (- (funcall parse-number) 1))
+           (col-end (1- (funcall parse-number)))
            (start (hdevtools//line-col-to-pos haskell-buffer line-start
                                               col-start))
            (end (hdevtools//line-col-to-pos haskell-buffer line-end col-end)))
-      (hdevtools//make-type-info
+      (hdevtools/make-type-info
        :start start
        :end end
+       ;; The +2 and 1- here are to skip over quotes.
        :type (buffer-substring (+ 2 (point)) (1- (line-end-position)))))))
 
 (defun hdevtools//line-col-to-pos (buffer line column)
